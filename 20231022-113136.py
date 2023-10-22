ad94b29df7fb97eaba153fb6a95dadfb415cb085
dzl2007 <142427567+dzl2007@users.noreply.github.com>
2023-10-22 11:36:11 +0800

    简易计算
    建议计算

---
+++ 20231022-113136.py
@@ -0,0 +1,17 @@
+print(" 算法： 1加 2减 3乘  4除以                        ")
+a =int(input("输入"))
+b =int(input("对象"))
+c =int(input("算法"))
+if   2>c>0:    
+    print(a+b)
+elif    3>c>1:    
+    print(a-b)
+elif    4>c>2:    
+    print(a*b)
+elif     5>c>3:    
+    print (a/b)
+print("完成")
+
+
+
+    
